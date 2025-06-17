d7d41134ace8f5aecac9a35408b0090a5e421e2b
ka-few <njorovista@gmail.com>
2025-06-17 17:07:40 +0300

    Add DOM modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+document.querySelector('main').remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML
+const header = document.querySelector('#victory')
+header.textContent = "Njoroge is the champion"
+
+
 // Write your code here!
